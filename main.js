84ba01ea618bb2dde508ed82c58de47db7f50296
leahmarke <lmarke@live.com>
2022-07-24 00:07:26 -0400

    finished first half

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
-// Use a do...while loop to console.log the numbers from 1 to 1000.
-const number = 100;
-let i = 1;
+// 1)Use a do...while loop to console.log the numbers from 1 to 1000.
+let result = "";
+let number = 0;
+// let i = 1;
 
 do {
-  console.log(i);
-  i++;
-} while (i <= number);
-
-// Create an object (with keys and values) called person with the following data:
+  number = number + 1;
+  result = `${result + number}, `;
+  // console.log(i);
+  // i++;
+} while (number < 1000);
+console.log(result);
+// 2)Create an object (with keys and values) called person with the following data:
 const person = {
   firstName: "Jane",
   lastName: "Doe",
   birthDate: "Jan 5, 1925",
   gender: "female",
 };
-// Create a function that logs out the keys of the object using Object.keys().
-console.log("The keys in person object:", Object.keys(person));
-// Create a function that logs out the keys and values of the object using Object.entries().
-console.log("The entries in person object:", Object.entries(person));
-
-// Create an arrayOfPersons that contains multiple "people" objects. You can simply copy/paste the person object you made above multiple times. Feel free to change the values to reflect multiple people you might have in your database.
+// 3)Create a function that logs out the keys of the object using Object.keys().
+const objectKeys = (object) => {
+  const keys = Object.keys(object);
+  console.log(keys);
+  keys.forEach((key) => {
+    console.log(key);
+  });
+};
+objectKeys(person);
+// console.log("The keys in person object:", Object.keys(person));
+// 4)Create a function that logs out the keys and values of the object using Object.entries().
+// console.log("The entries in person object:", Object.entries(person));
+const objectEntries = (object) => {
+  const entries = Object.entries(object);
+  entries.forEach((entry) => {
+    const key = entry[0];
+    const value = entry[1];
+    console.log(`${key}: ${value}`);
+  });
+  console.log(entries);
+};
+objectEntries(person);
+// 5)Create an arrayOfPersons that contains multiple "people" objects.
 
 const arrayOfPersons = [
   {
@@ -41,28 +61,99 @@ const arrayOfPersons = [
     gender: "male",
   },
 ];
-// Create a function that uses a for...of loop and an if statement to console.log the value associated with the key birthDate of each object if the birth year is an odd number.
+// 6)Create a function that uses a for...of loop and an if statement to console.log the value associated with the key birthDate of each object if the birth year is an odd number.
+// need to target birthDate key, check value with % to see if odd number
 
-// let text = "";
-// function oddBirthYear() {
-//   for (let x of arrayOfPersons) {
-//     console.log(x);
-//     let odds = arrayOfPersons.filter(n => n%2)
-//     if(birthyear == odds) {
-//     console.log(value of birthDate)}
-//   }
-//   text += x;
-//   console.log(x);
-// }
-// console.log(oddBirthYear);
+for (const person of arrayOfPersons) {
+  const birthYear = person.birthDate.slice(-4);
+  if (birthYear % 2 === 1) {
+    console.log(birthYear);
+    console.log(person.firstName);
+  }
+}
 
-// Use .map() to map over the arrayOfPersons and console.log() their information.
-// Use .filter() to filter the persons array and console.log 0only males in the array.
+// 7)Use .map() to map over the arrayOfPersons and console.log() their information.
+const personInfo = (arr) => {
+  const result = arr.map((item) => {
+    const firstName = item.firstName;
+    const lastName = item.lastName;
+    const birthDate = item.birthDate;
+    const gender = item.gender;
+    console.log(`${firstName} ${lastName} is a ${gender} born ${birthDate}.`);
+  });
+};
+personInfo(arrayOfPersons);
+// 8)Use .filter() to filter the persons array and console.log only males in the array.
+const malesOnly = (array) => {
+  const males = array.filter((item) => {
+    return item.gender === "male";
+  });
+  console.log(males);
+};
+malesOnly(arrayOfPersons);
 
-// Create a function that returns true if the value of birthDate is before Jan 1, 1990.
+// 9)Create a function that returns true if the value of birthDate is before Jan 1, 1990.
+const birthdayBefore = (array) => {
+  array.forEach((item) => {
+    const birthYear = item.birthDate.slice(-4);
+    console.log(birthYear < 1990);
+  });
+};
+birthdayBefore(arrayOfPersons);
 
-// Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
+// 10)Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
+const birthdayBefore1990 = (array) => {
+  const bornBefore1990 = array.filter((item) => {
+    const birthYear = item.birthDate.slice(-4);
+    console.log(typeof birthYear);
+    return birthYear < 1990;
+  });
+  console.log(bornBefore1990);
+};
+birthdayBefore1990(arrayOfPersons);
+// /////////////////////////////////////////////////////////////////////////////////////
+//? Pt. 2 From Scratch
 
-// BONUS - Create a function that returns true if the date passed to it is >= 21 years in the past.
+const arrayOfPeople = [
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    birthDate: "Aug 5, 1925",
+    gender: "female",
+  },
+  {
+    firstName: "Leah",
+    lastName: "Marke",
+    birthDate: "Jan 12, 1950",
+    gender: "female",
+  },
+  {
+    firstName: "Mark",
+    lastName: "Marksmith",
+    birthDate: "June 15, 2005",
+    gender: "male",
+  },
+];
+// 11) .find()
 
-// BONUS - .filter() out the people in the array who are younger than 21.
+// Code plan: will use the .find method to pull out the first female
+
+// Pseudo code: need to set a variable = array.find(takes a function/what I want to find)
+const firstFemale = (array) => {
+  const findFirstFemale = array.find((item) => {
+    return item.gender === "female";
+  });
+  console.log(findFirstFemale);
+};
+firstFemale(arrayOfPeople);
+
+// 12) .findIndex()
+// Code plan: use .findIndex to locate the index of the first person who's last name starts with an 'M'
+// Pseudo code: set variable = array.findIndex
+const lastNameStartsWith = (array) => {
+  const lastNameM = array.findIndex((item) => {
+    item.lastName.charAt(0) === "M";
+    console.log(lastNameM);
+  });
+};
+lastNameStartsWith(arrayOfPeople);
